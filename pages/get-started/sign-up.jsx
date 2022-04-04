08458c3b90caa87346197a3a46301113d85cbfa6
Catriel-Acevey <aceveycatriel@gmail.com>
2022-04-04 17:45:20 -0300

    I add next button in sign up

--- pages/get-started/sign-up.jsx
+++ pages/get-started/sign-up.jsx
@@ -1,14 +1,21 @@
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
-const LOGGED_IN = true;
+// const LOGGED_IN = true;
 
 function SignUp() {
   const router = useRouter();
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (loggedIn) {
+      router.push("/get-started/choose-plan");
+    }
+  }, [loggedIn]);
   return (
     <>
-      <button onClick={router.push("/get-started/choose-plan")}>NEXT</button>
-
       <h1>Soy Sign-Up</h1>
+      <button onClick={() => setLoggedIn(true)}>NEXT</button>
     </>
   );
 }
