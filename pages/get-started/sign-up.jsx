e968d11307875074119868b80de162695eff0397
Catriel-Acevey <aceveycatriel@gmail.com>
2022-04-08 10:35:22 -0300

    add styles on inputs when have wrong values

--- pages/get-started/sign-up.jsx
+++ pages/get-started/sign-up.jsx
@@ -1,24 +1,153 @@
+// import { useRouter } from "next/router";
+// import { useState, useEffect } from "react";
+
+// function SignUp() {
+//   const router = useRouter();
+
+//   return (
+//     <>
+//       <h1>Sign-Up</h1>
+//       <button onClick={() => router.push("/get-started/choose-plan")}>
+//         NEXT
+//       </button>
+//     </>
+//   );
+// }
+
+// export default SignUp;
+
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import ReCAPTCHA from "react-google-recaptcha";
+import TextError from "./TextError";
+import * as Yup from "yup";
 
-function SignUp() {
+const FormRegister = () => {
   const router = useRouter();
-  // const [loggedIn, setLoggedIn] = useState(false);
-
-  // useEffect(() => {
-  //   // if (loggedIn) {
-  //   router.push("/get-started/choose-plan");
-  //   // }
-  // }, [loggedIn]);
-
+  const [validCaptcha, setValidCaptcha] = useState(null);
+  const captcha = useRef(null);
+  const onChange = () => {
+    if (captcha.current.getValue()) {
+      setValidCaptcha(true);
+    }
+  };
+  const validate = Yup.object({
+    email: Yup.string()
+      .required("Please enter an email.")
+      .email("The email entered is not valid."),
+    password: Yup.string()
+      .required("Please enter a password.")
+      .matches(
+        /(?=.*[a-z])/,
+        "Password must contain at least one lowercase character."
+      )
+      .matches(
+        /(?=.*[A-Z])/,
+        "Password must contain at least one uppercase character."
+      )
+      .min(8, "Password must be at least 8 characters in length."),
+  });
+  const handleSubmit = (values) => {
+    if (captcha.current.getValue()) {
+      setValidCaptcha(true);
+      console.log("you registered successfully");
+      router.push("/get-started/choose-plan/");
+    } else {
+      setValidCaptcha(false);
+    }
+  };
   return (
     <>
-      <h1>Sign-Up</h1>
-      <button onClick={() => router.push("/get-started/choose-plan")}>
-        NEXT
-      </button>
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-md-4 offset-md-4">
+            <div className="login-form px-3 py-5">
+              <Formik
+                initialValues={{
+                  email: "",
+                  password: "",
+                }}
+                validationSchema={validate}
+                onSubmit={handleSubmit}
+              >
+                {({ errors, touched }) => (
+                  <Form>
+                    <div className="mb-3">
+                      <label className="form-label" htmlFor="email-register">
+                        Your Email
+                      </label>
+                      <Field
+                        className={`form-control ${
+                          errors.email && touched.email && "is-invalid"
+                        }`}
+                        type="email"
+                        id="email-register"
+                        name="email"
+                        placeholder="email@email.com"
+                      />
+                      <ErrorMessage
+                        name="email"
+                        component={() => <TextError text={errors.email} />}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label" htmlFor="password-register">
+                        Password
+                      </label>
+                      <Field
+                        className={`form-control ${
+                          errors.password && touched.password && "is-invalid"
+                        }`}
+                        type="password"
+                        id="password-register"
+                        name="password"
+                        placeholder="************"
+                      />
+                      <ErrorMessage
+                        name="password"
+                        component={() => <TextError text={errors.password} />}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <p>
+                        Your password must be at least 8 characters long with at
+                        least 1 capital letter
+                      </p>
+                    </div>
+                    <div className="captcha">
+                      <ReCAPTCHA
+                        ref={captcha}
+                        sitekey="6LexTJYeAAAAAO9cioYdAmJNEEwypY6_TDiPmv_V"
+                        onChange={onChange}
+                      />
+                    </div>
+                    {validCaptcha === false && (
+                      <div className="captcha">
+                        {/* <div className="mt-1 text-danger">
+                          {" "}
+                          please complete the captcha{" "}
+                        </div> */}
+                        <TextError text={"Please complete the captcha"} />
+                      </div>
+                    )}
+                    <div className="d-grid col-6 mx-auto mt-5">
+                      <button
+                        className="form-submit btn-register py-2 fs-5"
+                        type="submit"
+                      >
+                        Next
+                      </button>
+                    </div>
+                  </Form>
+                )}
+              </Formik>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
-}
+};
 
-export default SignUp;
+export default FormRegister;
