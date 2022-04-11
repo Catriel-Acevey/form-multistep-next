18d109fbd2c5e44b36bc6029cb6c499a9014f126
Catriel-Acevey <aceveycatriel@gmail.com>
2022-04-11 10:21:51 -0300

    apply ts to app and index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import { FormProvider } from "../context/formContext.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <FormProvider>
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import { NextPage } from "next";
+
 // You can use a query string to set the step and `router.push(href, as)` to handle the form steps within the same page but have different URLs displayed
-const Home = () => {
+const Home: NextPage = () => {
   return (
     <div>
       <h1>Hello word!</h1>
