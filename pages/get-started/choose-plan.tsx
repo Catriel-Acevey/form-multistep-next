a18f5c0c7c6272f59cc629df16575d28bf408874
Catriel-Acevey <aceveycatriel@gmail.com>
2022-04-18 09:58:34 -0300

    add styles from choose-plan

--- pages/get-started/choose-plan.tsx
+++ pages/get-started/choose-plan.tsx
@@ -2,16 +2,32 @@ import ChoosePlanForm from "../../components/ChoosePlanForm";
 
 function ChoosePlan() {
   return (
-    <>
+    <div className="get-started">
       <div className="container p-4">
         <div className="row">
-          <div className="col-md-4 offset-md-4 ">
-            <h1>Choose Plan</h1>
+          <div className="col-md-8 offset-md-1 ">
+            <div className="title">
+              <h1>Choose The Plan That Works For You</h1>
+            </div>
+            <div className="list">
+              <ul>
+                <li>Your first month is free.</li>
+                <li>You may cancel at any time.</li>
+              </ul>
+            </div>
             <ChoosePlanForm />
+            <div className="plan">
+              <img
+                className="me-5"
+                src="/images/pricingsprite-month.png"
+                alt="girl"
+              />
+              <img src="/images/pricingsprite-year.png" alt="kid" />
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
